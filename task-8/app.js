3fbef6b2725f9782f2dd19a2868ccb5faac4dab0
Md Tuser Sheikh <tuser.sheikh@zoho.com>
2022-09-24 18:21:40 +0600

    task 8 testing github env

--- task-8/app.js
+++ task-8/app.js
@@ -1,1 +1,16 @@
-console.log("hi");
+const btnSearchEl = document.getElementById("btn-search");
+const inputEl = document.getElementById("input");
+
+btnSearchEl.addEventListener("click", searchMovie);
+
+function searchMovie() {
+  const movieName = inputEl.value;
+
+  console.log(movieName);
+
+  console.log("1", process);
+  console.log("2", process.env);
+  console.log("3", process.env.OMDB_API_KEY);
+  console.log("4", env);
+  console.log("5", env.OMDB_API_KEY);
+}
