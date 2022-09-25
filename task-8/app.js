52277d48571a11fd9c3e647d2cf8ec4b75af198d
Md Tuser Sheikh <tuser.sheikh@zoho.com>
2022-09-25 19:06:20 +0600

    task 8 - add watchlist working

--- task-8/app.js
+++ task-8/app.js
@@ -1,6 +1,7 @@
 const btnSearchEl = document.getElementById("btn-search");
 const inputEl = document.getElementById("input");
 const searchResultEl = document.getElementById("search-result");
+const btnWatchlistEl = document.getElementsByClassName("btn-watchlist");
 
 btnSearchEl.addEventListener("click", searchMovie);
 
@@ -17,30 +18,31 @@ async function searchMovie() {
     return renderMovieNotFound();
   }
 
-  console.log("allMovies");
-  console.log(allMovies);
-  console.log("allMovies");
-
   for (let movie of allMovies.Search) {
     const responseMovie = await fetch("https://www.omdbapi.com/?apikey=800a17ec&i=" + movie.imdbID);
     const movieInfo = await responseMovie.json();
-    console.log(movieInfo);
 
     movies.push({
+      id: movieInfo.imdbID,
       poster: movieInfo.Poster,
       title: movieInfo.Title,
       rating: movieInfo.imdbRating,
       runtime: movieInfo.Runtime,
       genre: movieInfo.Genre,
       plot: movieInfo.Plot,
+      isWatchList: false,
     });
-
-    render();
   }
+  return render();
 }
 
 function render() {
   let searchResultHtml = "";
+  let localStorageWatchlistIds = JSON.parse(localStorage.getItem("movies")).map((m) => m.id);
+
+  movies = movies.map((m) => (localStorageWatchlistIds.includes(m.id) ? { ...m, isWatchList: true } : m));
+
+  console.log(movies);
 
   for (let movie of movies)
     searchResultHtml += `
@@ -60,10 +62,18 @@ function render() {
             <div class="runtime">${movie.runtime}</div>
             <div class="genre">${movie.genre}</div>
             <div class="watchlist">
-              <button id="watchlist">
-                <img src="images/plus-svg.svg" alt="plus svg icon">  
-                Watchlist
-              </button>
+              ${
+                movie.isWatchList
+                  ? `<button class="btn-watchlist">
+                  <img src="images/minus-svg.svg" alt="minus svg icon">  
+                  Remove
+                </button>`
+                  : `<button class="btn-watchlist">
+                  <img src="images/plus-svg.svg" alt="plus svg icon">  
+                  Watchlist
+                </button>`
+              }
+              
             </div>
           </div>
           <div class="plot">
@@ -74,6 +84,7 @@ function render() {
   `;
 
   searchResultEl.innerHTML = searchResultHtml;
+  enableWatchlistBtn();
 }
 
 function renderMovieNotFound() {
@@ -82,4 +93,26 @@ function renderMovieNotFound() {
       <p>Unable to find what you're looking for. Please try another search.</p>
     </div>
   `;
+}
+
+function enableWatchlistBtn() {
+  for (let i = 0; i < movies.length; i++) {
+    btnWatchlistEl[i].addEventListener("click", function () {
+      if (movies[i].isWatchList) {
+        // movies[i].isWatchList = false;
+      } else {
+        let watchlistMovies = [movies[i]];
+        let localStorageWatchlist = JSON.parse(localStorage.getItem("movies"));
+
+        if (localStorageWatchlist) {
+          watchlistMovies = [...watchlistMovies, ...localStorageWatchlist];
+        }
+
+        localStorage.setItem("movies", JSON.stringify(watchlistMovies));
+        movies[i].isWatchList = true;
+      }
+
+      render();
+    });
+  }
 }
